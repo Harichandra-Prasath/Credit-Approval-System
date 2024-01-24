f967a78eaaff53af06b6c78ce26eeb984306b3c4
Harichandra-Prasath <1hcp302004@gmail.com>
2024-01-24 10:23:44 +0530

    docker fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,13 @@
 #!bin/sh
-python manage.py makemigrations api
-python manage.py migrate --no-input
-python manage.py test
-python manage.py populate && python manage.py runserver 0.0.0.0:8000 && fg
+CONTAINER_FIRST_STARTUP="CONTAINER_FIRST_STARTUP"
+if [ ! -e /$CONTAINER_FIRST_STARTUP ]; then
+    touch /$CONTAINER_FIRST_STARTUP
+
+    #Below should run only for the first time while creating the container
+    python manage.py makemigrations api
+    python manage.py migrate --no-input
+    python manage.py test
+    python manage.py populate && python manage.py runserver 0.0.0.0:8000 && fg
+else
+    python manage.py runserver 0.0.0.0:8000
+fi
